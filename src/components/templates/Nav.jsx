00ddcfa5cb23262974e76515a5bc0cd0174d6c99
evertonnppp <44758876+evertonnppp@users.noreply.github.com>
2019-06-20 17:24:15 -0300

    Add files via upload

---
+++ src/components/templates/Nav.jsx
@@ -0,0 +1,20 @@
+import './Nav.css'
+import React from'react'
+
+export default props =>
+<aside className="menu-area">
+    <nav className="menu-area">
+        {/* */}
+           <a href="#/">
+               <i className="fa fa-home">
+                   Inicio
+               </i>
+           </a>
+           <a href="#/users">
+               <i className="fa fa-users">
+                   Usuarios
+               </i>
+           </a>
+    </nav>
+
+</aside>
